759e2353113d87db1ad110728dfc626147ad5fa1
Herculys Douglas <herculysdouglas@hotmail.com>
2019-01-07 08:49:52 -0300

    Add files via upload
    É uma tela inicial em que é possível iniciar alguns conceitos relacionados a criação de uma tela simples.

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PrimeiroApp
+//
+//  Created by Macintosh on 30/10/16.
+//  Copyright © 2016 Macintosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //exibir uma mensagem
+        print( "Meu primeiro APP" )
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
